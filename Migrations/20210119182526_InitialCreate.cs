e7bd418e44882b378a524b9623385d12c04e67a4
Jannik Maag <jannikmaag@outlook.com>
2021-01-19 20:59:21 +0100

    add vue and webpack

--- Migrations/20210119182526_InitialCreate.cs
+++ Migrations/20210119182526_InitialCreate.cs
@@ -8,6 +8,24 @@ namespace ShopLocator.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    Housenumber = table.Column<int>(nullable: false),
+                    Zipcode = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    Phonenumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Todo",
                 columns: table => new
                 {
@@ -24,6 +42,9 @@ namespace ShopLocator.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Store");
+
             migrationBuilder.DropTable(
                 name: "Todo");
         }
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./wwwroot/vue/app.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+}
