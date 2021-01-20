7045a9d380c08425f899f76221d1dfba5cc7f585
Jannik Maag <jannikmaag@outlook.com>
2021-01-20 10:57:58 +0100

    Remove test-case scaffolds

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -14,10 +14,6 @@ namespace ShopLocator.Models
         {
         }
 
-        public DbSet<ShopLocator.Models.Todo> Todo { get; set; }
-
         public DbSet<ShopLocator.Models.Store> Store { get; set; }
-
-        public DbSet<ShopLocator.Models.Shop> Shop { get; set; }
     }
 }
